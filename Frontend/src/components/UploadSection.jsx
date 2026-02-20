9277b97d4666e1f761c5a342dc63196b1b87e77f
Alyan5 <alyanmuhammad466@gmail.com>
2026-02-21 02:36:37 +0500

    lower confidence threshold to 65% to stop rejecting normal X-rays

--- Frontend/src/components/UploadSection.jsx
+++ Frontend/src/components/UploadSection.jsx
@@ -64,7 +64,7 @@ const UploadSection = () => {
 
     try {
       const data = await predictDisease(selectedFile);
-      if (data.confidence < 0.85) {
+      if (data.confidence < 0.65) {
         setResult({ _lowConfidence: true });
       } else {
         setResult(data);
